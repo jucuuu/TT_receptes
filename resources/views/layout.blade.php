18191785d3db9fc01394ec1cebe014b2f25438f9
jucuuu <curakovajj@gmail.com>
2023-06-15 20:01:55 +0300

    Tailwind CSS installation done

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,10 +5,13 @@
     <meta name="viewport" content="width=device-width
     initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
     <title>JH Recipes</title>
 </head>
 <body>
-    <h1>JH Recipes</h1>
+    <h1 class="text-3xl font-bold underline">
+        JH Recipes
+    </h1>
     {{-- VIEW OUTPUT --}}
     @yield('content')
 </body>
