7d4ab65f07af8c9f6a6a98d800d4c55e19fbd445
jucuuu <curakovajj@gmail.com>
2023-06-22 03:27:06 +0300

    Cover image validation

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Intervention\Image\Facades\Image;
 use Illuminate\Http\Request;
 use App\Models\Recipe;
 use Illuminate\Support\Facades\Auth;
